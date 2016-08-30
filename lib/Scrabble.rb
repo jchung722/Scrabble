c0887356b0b460e1b902d094af7b0d0a28366cca
Jessica <jchung722@gmail.com>
2016-08-30 11:34:11 -0700

    Scrabble Wave 1

--- lib/Scrabble.rb
+++ lib/Scrabble.rb
@@ -1,1 +1,52 @@
 #Scrabble.rb
+
+require_relative '../Scrabble/'
+require 'awesome_print'
+class Scrabble::Scoring
+
+
+  def self.score(word)
+    word.upcase!
+    letters = word.split(//)
+    score = 0
+    letters.each do |letter|
+      Scrabble::LETTER_VALUES.each do |key, value|
+        if key.include?(letter)
+          score += value
+        end
+      end
+    end
+
+    if letters.length == 7
+      score += 50
+    end
+
+    return score
+
+  end
+
+  def self.highest_score_from(word_options)
+
+    scores = []
+    word_options.each do |word|
+      scores << score(word)
+    end
+    #i = scores.index(scores.max)
+    max_val = scores.max
+    index_array = scores.each_index.select{|i| scores[i] == max_val}
+
+    high_score_words = word_options.values_at(*index_array)
+
+    if high_score_words.max_by(&:length).length == 7 # (&:length) is a shortcut of the iteration {|i| i.length}
+      return high_score_words.max_by(&:length)
+    else
+      return high_score_words.min_by(&:length)
+    end
+
+  end
+
+end
+
+ap Scrabble::Scoring.highest_score_from(["qzqzqj", "aeiould"])
+# ap Scrabble::Scoring.score("aeiould")
+# ap Scrabble::Scoring.score("qzqzqj")
--- specs/Scrabble_spec.rb
+++ specs/Scrabble_spec.rb
@@ -1,1 +1,33 @@
 #Scrabble_spec.rb
+
+require_relative 'Spec_helper'
+require_relative '../lib/Scrabble'
+
+describe 'Testing Scrabble' do
+
+  it "Must return total score for given word" do
+    expect(Scrabble::Scoring.score("art")).must_equal(3)
+    expect(Scrabble::Scoring.score("quit")).must_equal(13)
+  end
+
+  it "Must return total score including bonus for seven letter word" do
+    expect(Scrabble::Scoring.score("racecar")).must_equal(61)
+  end
+
+  it "Must return word with highest score from array of words" do
+    expect(Scrabble::Scoring.highest_score_from(["art","quiz","racecar"])).must_equal("RACECAR")
+  end
+
+  it "In a tie, must return word with fewer letters" do
+    expect(Scrabble::Scoring.highest_score_from(["goal", "roast"])).must_equal("GOAL")
+  end
+
+  it "In a tie, must return word that used seven letters" do
+    expect(Scrabble::Scoring.highest_score_from(["qzqzqj", "aeiould"])).must_equal("AEIOULD")
+  end
+
+  it "In a tie, with same length words, must return first word in list" do
+    expect(Scrabble::Scoring.highest_score_from(["pole", "same"])).must_equal("POLE")
+  end
+
+end
